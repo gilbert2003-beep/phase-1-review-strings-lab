68765ed189d8df81a48b6047f58c5f43be911b7f
gilbert <gilbert.bageni@student.moringaschool.com>
2023-06-17 13:15:38 +0300

    done

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Write your code in this file!
-const currentUser = 'Grace Hopper';
-
-
-const welcomeMessage = "Welcome to Flatbook, "+"Grace Hopper!";
-let excitedWelcomeMessage= "WELCOME TO FLATBOOK, "+"GRACE HOPPER!";
-let shortGreeting="Welcome, "+ "G!";
-
-
-
+const currentUser = "Grace Hopper";
+const welcomeMessage="Welcome to Flatbook, "+"Grace Hopper!";
+let excitedWelcomeMessage="WELCOME TO FLATBOOK, " + "GRACE HOPPER!";
+let shortGreeting= "Welcome, " + "Grace hoppper!" 
